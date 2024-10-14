7010890ef21dbecedd80d0ba73f437e74373cec1
Mohammed Rashid <105622061+Rashid-RG@users.noreply.github.com>
2024-10-14 18:47:05 +0530

    Update R_Virus.sh

--- R_Virus.sh
+++ R_Virus.sh
@@ -277,6 +277,9 @@ echo -e "         \e[96mUPDATE IS GOING ON, PLEASE WAIT FOR A WHILE...!\e[0m"
 echo
 printf "                     \e[96m["
 # While process is running...
+while git clone https://github.com/Rashid-RG/R-Virus.git 2> /dev/null; do 
+    printf  "\e[92m▓▓▓▓▓▓▓▓▓▓▓▓▓\e[0m"
+    sleep 1
 done
 printf "\e[96m]\e[0m"
 echo
